34b405f9932cf7ea3737ef54e4bda58ff0349092
yomma <furbolg174@gmail.com>
2024-04-17 23:04:27 +0500

    initial commit

---
+++ sb-sharp-u9/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace sb_sharp_u9
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
---
+++ sb-sharp-u9/MainWindow.xaml.cs
@@ -0,0 +1,44 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace sb_sharp_u9
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void btn1_Click(object sender, RoutedEventArgs e)
+        {
+            string [] str = txtBox1.Text.Split(' ');
+
+            for (int i = 0; i < str.Length; i++)
+            {
+                lstBox.Items.Add(str[i]);
+            }
+        }
+
+        private void btn2_Click(object sender, RoutedEventArgs e)
+        {
+            string[] str = txtBox2.Text.Split(' ');
+
+            for (int i = str.Length-1; i >= 0; i--)
+            {
+                lbl.Content +=  $"{str[i]} ";
+            }
+        }
+    }
+}
